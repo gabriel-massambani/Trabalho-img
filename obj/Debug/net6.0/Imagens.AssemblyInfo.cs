fbe73254d9cdf981dcf0f201bf90cecb55025753
gabriel <110850579+gabriel-massambani@users.noreply.github.com>
2024-05-20 18:56:58 -0300

    alteracao

--- obj/Debug/net6.0/Imagens.AssemblyInfo.cs
+++ obj/Debug/net6.0/Imagens.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Imagens")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8abc10577b643414e9c90d65eff7f4c0df667277")]
 [assembly: System.Reflection.AssemblyProductAttribute("Imagens")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Imagens")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
